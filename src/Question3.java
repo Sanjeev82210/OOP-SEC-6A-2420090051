dd6e4a3f88abe22296ae3ca0fc3e1b6193f1b75e
sanjeev82210 <2420090051@klh.edu.in>
2025-08-06 09:41:17 +0530

    Pratical Lab Problems Codes

---
+++ src/Question3.java
@@ -0,0 +1,11 @@
+public class Question3 {
+    public static void main(String[] args) {
+        int i = 1000;
+        long l = i;
+        float f = l;
+
+        System.out.println("Int: " + i);
+        System.out.println("Long: " + l);
+        System.out.println("Float: " + f);
+    }
+}
